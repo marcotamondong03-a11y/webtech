6483b49735bbb1b1640248181c81dca0d7f40190
marcotamondong03-a11y <marcotamondong03@gmail.com>
2026-05-05 21:22:42 +0800

    Add files via upload

---
+++ Zeus Files/Zeus.js
@@ -0,0 +1,49 @@
+      var groupContainer = document.getElementById("Home");
+      if (groupContainer) {
+        groupContainer.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='Top']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        })
+      }
+      
+      var groupContainer = document.getElementById("Profile");
+      if (groupContainer) {
+        groupContainer.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='whoAmI1']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        })
+      }
+
+       var groupContainer = document.getElementById("Contacts");
+      if (groupContainer) {
+        groupContainer.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='lets-get-in-touch']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        })
+      }
+
+        var groupContainer = document.getElementById("Works");
+      if (groupContainer) {
+        groupContainer.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='Pubmats']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        })
+      }
+
+        var groupContainer = document.getElementById("Home");
+      if (groupContainer) {
+        groupContainer.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='Personal website assets/Home pic 3.png']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        })
+      }
